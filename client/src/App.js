b050e631733d3e678c06037d9494cb7d42bafaeb
naekang <66901361+naekang@users.noreply.github.com>
2021-05-08 17:06:09 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -39,9 +39,22 @@ props or state => shouldComponentUpdate()
 */
 
 class App extends Component {
-  state = {
-    customers: '',
-    completed: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0,
+    };
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0,
+    });
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
   };
 
   componentDidMount() {
@@ -110,7 +123,7 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd />
+        <CustomerAdd stateRefresh={this.stateRefresh} />
       </div>
     );
   }
